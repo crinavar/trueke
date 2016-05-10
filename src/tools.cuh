e8a2ec144718c4f31adecf073030c86aad0df2f2
Cristobal Navarro <crinavar@dcc.uchile.cl>
2016-05-10 16:40:28 -0300

    debug to find multi-GPU problem

--- src/tools.cuh
+++ src/tools.cuh
@@ -587,6 +587,9 @@ void accum_realization_statistics( setup_t *s, int tid, int a, int b, int realiz
 		if( s->obstable[q].rdata[C_POS].n == 0 ){
 			s->obstable[q].rdata[C_POS].x1 = val;
 		}
+        printf("tid=%i   k=%i  T=%f    C=%f\n", tid, k, T, val);
+        getchar();
+
 		variance_step(val, &(s->obstable[q].rdata[C_POS].n), &(s->obstable[q].rdata[C_POS].mean), &(s->obstable[q].rdata[C_POS].w1), &(s->obstable[q].rdata[C_POS].w2), s->obstable[q].rdata[C_POS].x1, 
 						&(s->obstable[q].rdata[C_POS].lastx));
 
